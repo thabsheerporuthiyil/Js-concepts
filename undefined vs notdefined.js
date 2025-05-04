ab8028db0cd21cfe04ca44d594f5be80d048090a
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-05-04 11:43:28 +0530

    Create undefined vs notdefined.js

---
+++ undefined vs notdefined.js
@@ -0,0 +1,6 @@
+//Not defined is something which has not allocated memory
+//Undefined is a variable has been declared, but no value has been assigned to it yet.
+
+console.log(a);
+var a=10;
+console.log(x);
