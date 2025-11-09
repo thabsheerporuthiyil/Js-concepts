e4a20363cd8e25e0c617bed7c0b37571624ecdf1
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-11-09 21:12:00 +0530

    Delete undefined vs notdefined.js

--- undefined vs notdefined.js
+++
@@ -1,6 +0,0 @@
-//Not defined is something which has not allocated memory
-//Undefined is a variable has been declared, but no value has been assigned to it yet.
-
-console.log(a);
-var a=10;
-console.log(x);
