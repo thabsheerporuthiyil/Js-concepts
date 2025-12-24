3b442b3cb8d0b8f2762abea5c1404d12376b5294
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-12-24 23:16:53 +0530

    Delete Problems/palindrome.js

--- Problems/palindrome.js
+++
@@ -1,6 +0,0 @@
-function isPalindrome(str){
-    str=str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let reverse=str.split('').reverse().join('');
-    return str===reverse;
-}
-console.log(isPalindrome("malayalam"));
